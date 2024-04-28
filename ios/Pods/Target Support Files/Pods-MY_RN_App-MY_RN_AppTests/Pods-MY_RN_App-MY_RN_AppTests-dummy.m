b009e16d30658e912eae927df9b6ba9404f102dd
Chandravir Parihar <chandravir.parihar@wolterskluwer.com>
2024-04-28 23:15:48 +0530

    Initial commit

---
+++ ios/Pods/Target Support Files/Pods-MY_RN_App-MY_RN_AppTests/Pods-MY_RN_App-MY_RN_AppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MY_RN_App_MY_RN_AppTests : NSObject
+@end
+@implementation PodsDummy_Pods_MY_RN_App_MY_RN_AppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-MY_RN_App/Pods-MY_RN_App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MY_RN_App : NSObject
+@end
+@implementation PodsDummy_Pods_MY_RN_App
+@end
